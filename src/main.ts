1b825ee202d5a936571b196ec9debe203bb56721
Marcus Ball <marcus.ball@live.com>
2021-05-13 19:50:39 -0400

    Get spectre.css working again

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 
+// Add Spectre.css
+import "./assets/sass/main.scss";
+
 createApp(App).use(router).mount('#app')
